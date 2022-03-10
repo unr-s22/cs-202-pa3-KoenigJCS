a45d3da673b9d3d1bbb8d31fd91dcf668d86d96b
KoenigJCS <jkoenig@nevada.unr.edu>
2022-03-10 12:37:19 -0800

    Jkoenig Money v1

--- Money.cpp
+++ Money.cpp
@@ -1,1 +1,89 @@
 //Money.cpp
+#include "Money.h"
+
+Money::Money(int dollar, int cents):m_dollars(dollar),m_cents(cents){};
+Money::Money():m_dollars(0),m_cents(0){};
+
+std::ostream& operator <<(std::ostream& os, const Money& money)
+{
+    os<<'$';
+    if(money.m_dollars==0&&money.m_cents<0)
+    {
+        os<<'-'<<money.m_dollars;
+    }
+    else
+    {
+        os<<money.m_dollars;
+    }
+
+    if((money.m_cents>-10 && money.m_cents<0)||(money.m_cents<10&& money.m_cents>0))
+    {
+        os<<".0";
+    }
+    else
+    {
+        os<<'.';
+    }
+
+    if(money.m_cents<0)
+    {
+        os<<money.m_cents*-1;
+    }
+    else
+    {
+        os<<money.m_cents;
+    }
+
+    return os;
+}
+bool operator ==(const Money& money1, const Money& money2)
+{
+    return money1.m_dollars==money2.m_dollars && money1.m_cents==money2.m_cents;
+}
+
+bool operator >=(const Money& money1, const Money& money2)
+{
+    return(money1.m_dollars>money2.m_dollars||(money1.m_dollars==money2.m_dollars&&money1.m_cents>=money2.m_cents));
+}
+
+bool operator <=(const Money& money1, const Money& money2)
+{
+    return(money1.m_dollars<money2.m_dollars||(money1.m_dollars==money2.m_dollars&&money1.m_cents<=money2.m_cents));
+}
+
+bool operator !=(const Money& money1, const Money& money2)
+{
+    return money1.m_dollars!=money2.m_dollars||money1.m_cents!=money2.m_cents;
+}
+
+bool operator >(const Money& money1, const Money& money2)
+{
+    return(money1.m_dollars>money2.m_dollars||(money1.m_dollars==money2.m_dollars&&money1.m_cents>money2.m_cents));
+}
+
+bool operator <(const Money& money1, const Money& money2)
+{
+    return(money1.m_dollars<money2.m_dollars||(money1.m_dollars==money2.m_dollars&&money1.m_cents<money2.m_cents));
+}
+
+Money operator +(const Money& money1, const Money& money2)
+{
+    Money m0(money1.m_dollars+money2.m_dollars,money1.m_cents+money2.m_cents);
+    if(m0.m_cents>=100)
+    {
+        m0.m_cents-=100;
+        m0.m_dollars++;
+    }
+    return m0;
+}
+
+Money operator -(const Money& money1, const Money& money2)
+{
+    Money m0(money1.m_dollars-money2.m_dollars,money1.m_cents-money2.m_cents);
+    if(m0.m_cents>=0 && m0.m_dollars<0)
+    {
+        m0.m_cents-=100;
+        m0.m_dollars++;
+    }
+    return m0;
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
 //main.cpp
+#include "Money.h"
+#include "Account.h"
+
+int main()
+{
+
+}
