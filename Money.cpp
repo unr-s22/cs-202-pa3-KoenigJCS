85cf40942d38f9f1b614937d71621de620d9ad88
KoenigJCS <jkoenig@nevada.unr.edu>
2022-03-11 10:00:51 -0800

    Jkoenig Money v1.2

--- Money.cpp
+++ Money.cpp
@@ -6,39 +6,16 @@ Money::Money():m_dollars(0),m_cents(0){};
 
 std::ostream& operator <<(std::ostream& os, const Money& money)
 {
-    os<<'$';
-    if(money.m_dollars==0&&money.m_cents<0)
+    os<<'$'<<money.m_dollars+(money.m_cents*.01);
+    if(money.m_cents%10==0)
     {
-        os<<'-'<<money.m_dollars;
+        os<<'0';
     }
-    else
-    {
-        os<<money.m_dollars;
-    }
-
-    if((money.m_cents>-10 && money.m_cents<0)||(money.m_cents<10&& money.m_cents>0))
-    {
-        os<<".0";
-    }
-    else
-    {
-        os<<'.';
-    }
-
-    if(money.m_cents<0)
-    {
-        os<<money.m_cents*-1;
-    }
-    else
-    {
-        os<<money.m_cents;
-    }
-
     return os;
 }
 bool operator ==(const Money& money1, const Money& money2)
 {
-    return money1.m_dollars==money2.m_dollars && money1.m_cents==money2.m_cents;
+    return (money1.m_dollars==money2.m_dollars) && (money1.m_cents==money2.m_cents);
 }
 
 bool operator >=(const Money& money1, const Money& money2)
@@ -80,12 +57,7 @@ Money operator +(const Money& money1, const Money& money2)
 Money operator -(const Money& money1, const Money& money2)
 {
     Money m0(money1.m_dollars-money2.m_dollars,money1.m_cents-money2.m_cents);
-    if(m0.m_cents>0 && m0.m_dollars<0)
-    {
-        m0.m_cents-=100;
-        m0.m_dollars++;
-    }
-    if(m0.m_cents<0 && m0.m_dollars>0)
+    if(m0.m_cents<0)
     {
         m0.m_cents+=100;
         m0.m_dollars--;
